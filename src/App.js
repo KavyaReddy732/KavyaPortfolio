acb4fe805e80b24550670449dddda8e312e76cd5
Kavya Reddy <kavyareddy@Kavyas-MacBook-Air.local>
2021-09-13 22:09:16 +0200

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import { HashLink as Link } from 'react-router-hash-link';
-import About from "./components/About";
-import Home from "./components/Home";
-import Experience from './components/Experience';
-import Contact from './components/Contact';
+import resume from './images/Kavyacv.pdf';
+import image from './images/bg2.jpg'
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { HashLink as Link } from 'react-router-hash-link';
+// import About from "./components/About";
+// import Home from "./components/Home";
+// import Experience from './components/Experience';
+// import Contact from './components/Contact';
 import './App.css';
 import Footer from './components/Footer';
 
 function App() {
   return (
-  <div className="App">
-    <Router>
+    <div className="App">
+      <div className="bg">
+        <img src={image} id="bg" alt="bg"></img>
+        <h1 className='text'>
+          {`Kavya Konuganti `}
+        </h1>
+        <p className='text2'>
+          {`I'm a web Developer`}
+        </p>
+        <a className="button" href={resume} rel="noreferrer" target="_blank"><button>CV</button></a>
+      </div>
+      {/* <Router>
         <div className="bg">
           <nav className="nav_bar">
             <Link smooth to="#Home" className="Navi Name">Kavya Konuganti</Link>
@@ -26,8 +38,8 @@ function App() {
         <About />
         <Experience />
         <Contact />
-        <Footer/>
-    </Router>
+        <Footer/> </Router>*/}
+
   </div>
   );
 }
